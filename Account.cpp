c6e05e2c9760045118d779d518b331ed54680276
Guangzhuan <quan.fin.mo@gmail.com>
2018-06-02 15:14:09 -0400

    fixed the initialization of private const data member, it was the problem of declaration order and initialization order, they should have the same order, and it doesn't matter if the initial value is provide by the parameter list or not

--- Account.cpp
+++ Account.cpp
@@ -1,73 +1,66 @@
 #include "Account.h"
 
 using namespace std;
-// template <typename T>
-// Account<T>::Account(double totalBalance, 
-// 			double marginAvailable,
-// 			std::string currency,
-// 			T accountID,
-// 			double marginRate) :
-// 			Account<T>::Account(totalBalance, 
-// 					0, 0, 0,
-// 					marginAvailable,
-// 					0, 
-// 					currency,
-// 					accountID,
-// 					marginRate){}
+template <typename T>
+Account<T>::Account(double totalBalance, 
+			double marginAvailable,
+			std::string currency,
+			T accountID,
+			double marginRate) :
+			Account<T>::Account(totalBalance, 
+					0, 0, 0,
+					marginAvailable,
+					0, 
+					currency,
+					accountID,
+					marginRate){}
 
-// template<typename T>
-// Account<T>::Account(double totalBalance,
-// 			double unrealisePnL,
-// 			double realisePnL,
-// 			double marginUsed,
-// 			double marginAvailable,
-// 			long openTrades,
-// 			std::string currency,
-// 			T accountID, 
-// 			double marginRate):
-// // initiate the const data member via 
-// // initiate list, same effect as final 
-// // variable in Java.
-// 	_totalBalance (totalBalance),
-// 	_unrealisePnL(unrealisePnL),
-// 	_realisePnL(realisePnL),
-// 	_marginUsed(marginUsed),
-// 	_marginAvailable (marginAvailable),
-// 	_openTrades(openTrades),
-// 	_currency (currency),
-// 	_accountID (accountID),
-// 	_marginRate (marginRate) 
-// 	{
-// 		_amountAvailableRatio = _marginAvailable / _totalBalance;
-// 		_netAssetValue = _marginUsed + _marginAvailable;
-// 	}
-
-
-/* does c++ provide hashcode? 
 template<typename T>
-	Account<T>::calcHashCode(){
+Account<T>::Account(double totalBalance,
+			double unrealisePnL,
+			double realisePnL,
+			double marginUsed,
+			double marginAvailable,
+			long openTrades,
+			std::string currency,
+			T accountID, 
+			double marginRate):
+// initiate the const data member via 
+// initiate list, same effect as final 
+// variable in Java.
+// be care full about the order of the declaration 
+// of the data member,
+// the order of these variables in the initialization list
+// should be the same as the order of the declaration.
+// and it doesn't have to use the variables/values
+// of the parameter list.
+	_totalBalance (totalBalance),
+	_unrealisePnL(unrealisePnL),
+	_realisePnL(realisePnL),
+	_marginUsed(marginUsed),
+	_marginAvailable (marginAvailable),
+	_openTrades(openTrades),
+	_currency (currency),
+	_accountID (accountID),
+	_marginRate (marginRate),
+	_netAssetValue(_marginUsed + _marginAvailable),
+	_amountAvailableRatio (_marginAvailable / _totalBalance),
+	_hash (calcHashCode())
+	{}
+
+
+/* does c++ provide hashcode? */
+template<typename T> 
+int Account<T>::calcHashCode(){
 		const int prime = 31;
 		int result = 1;
-		result = prime * result + 
-				((_accountID == nullptr) ? 0 : _accountID.)
-	}
+		result = prime * result + 10;
+				// ((_accountID == nullptr) ? 0 : _accountID.)
+	return result;		
+}
 
-*/
+
 void testAccount(){
 	Account<string> account(123.0, 123.0, "USD", "123345", 345.0 );
 }
 
-// const double _totalBalance;
-// 	const double _unrealisePnL; // Profit & Loss
-// 	const double _realisePnL;
-// 	const double _marginUsed;
-// 	const double _marginAvailable;
-// 	const double _netAssetValue;
-// 	const long _openTrades;
-// 	const double _marginRate; // the leverage offered on this account, 
-// 								//for e.g. 0.05, 0.1 etc
-// 	const std::string _currency;
-// 	const T _accountID;
-// 	const std::string _toStr;
-// 	const _amountAvailableRatio; // the amount available to trade as 
-// 							
--- Account.h
+++ Account.h
@@ -23,35 +23,23 @@ public:
 			T accountID, 
 			double marginRate);
 private:
-	// why these private const variables?
-	// these const variables are immutable, 
-	// therefore we can calculate the hashCode() 
-	// and toString() values upfront in the constructors
-	// and store them in const variables,
-	// any call to toString() or hashCode() will return the pre-calculate
-	// values
-	//
-	// but does it necessary to have hashCode()
-	// and toString() in C++?
-	// what is the meaning of having these two method 
-	// in java?
 	const double _totalBalance;
 	const double _unrealisePnL; // Profit & Loss
 	const double _realisePnL;
 	const double _marginUsed;
 	const double _marginAvailable;
-	const double _netAssetValue;
 	const long _openTrades;
-	const double _marginRate; // the leverage offered on this account, 
-								//for e.g. 0.05, 0.1 etc
 	const std::string _currency;
 	const T _accountID;
-	const std::string _toStr;
+	const double _marginRate; // the leverage offered on this account, 
+								//for e.g. 0.05, 0.1 etc
+	const double _netAssetValue;
+	 // std::string _toStr;
 	const double _amountAvailableRatio; // the amount available to trade as 
 									// a fraction of total amount
-	// const int hash;
+	const int _hash;
 
-	// int calcHashCode();
+	int calcHashCode();
 };
 
 void testAccount();
